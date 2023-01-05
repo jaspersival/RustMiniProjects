936e39fddc83bcc12d73ae3cc213d3b0647cba9b
Jasper Sival <jasper.sival@alliander.com>
2023-01-05 14:58:14 +0100

    Refactor using the builder pattern for making the commands

--- triangle/src/main.rs
+++ triangle/src/main.rs
@@ -6,9 +6,36 @@ struct Args {
     ///Number of * elements in the top row
     top_number: usize,
 }
+
+#[derive(clap::Subcommand)]
+enum Action {
+    Top,
+    Bottom
+}
+
 fn main() {
-    let args = Args::parse();
-    let mut i = args.top_number;
+    let cmd = clap::Command::new("triangle").bin_name("triangle")
+        .subcommand_required(true)
+        .subcommand(clap::command!("top").arg(
+            clap::Arg::new("NUMBER")
+                .value_parser(clap::value_parser!(usize))
+        ))
+        .subcommand(clap::command!("bottom").arg(
+            clap::Arg::new("NUMBER")
+                .value_parser(clap::value_parser!(usize))
+        ));
+    let matches = cmd.get_matches();
+    let matches = match matches.subcommand() {
+        Some(("top", matches)) => matches,
+        Some(("bottom", matches)) => matches,
+        _ => unreachable!("clap should ensure we don't get here")
+    };
+    let number = matches.get_one::<usize>("NUMBER").expect("Please provide a NUMBER");
+    get_top_triangle(number);
+}
+
+fn get_top_triangle(number: &usize) {
+    let mut i = *number;
     while i > 0 {
         let row = "*".repeat(i);
         println!("{}", row);
